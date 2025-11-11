3d14b524c6cc1424e7defd2f7cacfe4cbc22edf7
sttuffe <sttuffe@gmail.com>
2025-11-12 00:41:26 +0900

    feat: 내역 UI 추가

--- app/src/main/java/com/sttuffe/expensetracker/ListScreen.kt
+++ app/src/main/java/com/sttuffe/expensetracker/ListScreen.kt
@@ -1,26 +1,57 @@
 package com.sttuffe.expensetracker
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import java.text.DecimalFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @Composable
 fun ListScreen(
     onNavigateToAddScreen: () -> Unit
 ) {
     //TODO: testData
-    val testData = List(30) { "testData $it" }
+    val testData = listOf(
+        TransactionLog(
+            id = 1,
+            type = TransactionType.EXPENSE,
+            amount = 2000,
+            content = "test1",
+            date = LocalDateTime.now()
+        ),
+        TransactionLog(
+            id = 2,
+            type = TransactionType.INCOME,
+            amount = 300000000000000,
+            content = "test2test2test2test2test2test2",
+            date = LocalDateTime.now().minusDays(5) // 5일 전
+        ),
+        TransactionLog(
+            id = 3,
+            type = TransactionType.EXPENSE,
+            amount = 1000,
+            content = "test3",
+            date = LocalDateTime.now().minusDays(1) // 어제
+        )
+    )
+    val sortedData = testData.sortedByDescending { it.date }
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -40,20 +71,70 @@ fun ListScreen(
                 .fillMaxSize()
                 .padding(innerPadding),
         ) {
-            items(testData) { item ->
+            items(sortedData) { item ->
+                TransactionRow(log = item)
+            }
+        }
+    }
+}
+
+@Composable
+fun TransactionRow(log: TransactionLog) {
+    // 날짜 포맷
+    val dateFormatter = DateTimeFormatter.ofPattern("MM.dd")
+    val dateString = log.date.format(dateFormatter)
+
+    // 금액 포맷
+    val decimalFormat = DecimalFormat("#,###")
+    val amountString = decimalFormat.format(log.amount) + "원"
+
+    Column {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 16.dp, horizontal = 20.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // 날짜
+            Text(
+                text = dateString,
+                fontSize = 16.sp,
+                color = Color.Gray,
+                modifier = Modifier.padding(end = 12.dp)
+            )
+
+            // 내용
+            Text(
+                text = log.content,
+                fontSize = 16.sp,
+                color = Color.Black,
+                maxLines = 1,
+                overflow = androidx.compose.ui.text.style.TextOverflow.Ellipsis, //  ... 처리
+                modifier = Modifier.weight(1f) //
+            )
+
+            // 금액
+            if (log.type == TransactionType.INCOME) {
+                //수입
                 Text(
-                    text = item,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(20.dp),
-                    fontSize = 18.sp
+                    text = "+ $amountString",
+                    color = Color(0xFF01579B),
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(start = 12.dp)
                 )
-                // 구분선
-                androidx.compose.material3.HorizontalDivider(
-                    color = Color.LightGray
+            } else {
+                //지출
+                Text(
+                    text = "- $amountString",
+                    color = Color(0xFFB71C1C),
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(start = 12.dp)
                 )
             }
         }
+        HorizontalDivider(color = Color.LightGray, thickness = 0.5.dp)
     }
 }
 
