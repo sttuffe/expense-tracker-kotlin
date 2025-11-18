2c139fe688be2244b2a6e9ec14c7a9932f85bb1f
sttuffe <sttuffe@gmail.com>
2025-11-18 23:31:57 +0900

    feat: 첫 구동 시 예시용 초기 데이터 추가 로직 추가

--- app/src/main/java/com/sttuffe/expensetracker/data/TransactionDatabase.kt
+++ app/src/main/java/com/sttuffe/expensetracker/data/TransactionDatabase.kt
@@ -5,6 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.sttuffe.expensetracker.TransactionLog
 
 @Database(
@@ -28,12 +29,33 @@ abstract class TransactionDatabase : RoomDatabase() {
                     TransactionDatabase::class.java,
                     "transaction_database"
                 )
+                    .addCallback(object : RoomDatabase.Callback() {
+                        override fun onCreate(db: SupportSQLiteDatabase) {
+                            super.onCreate(db)
+                            fillInitialData(db)
+                        }
+                    })
                     .fallbackToDestructiveMigration() //TODO
                     .build()
 
                 INSTANCE = instance
                 instance
             }
+        }
+
+        // 첫 구동 시 예시용 초기 데이터 추가 함수
+        private fun fillInitialData(db: SupportSQLiteDatabase) {
+            val now = System.currentTimeMillis()
+
+            db.execSQL(
+                "INSERT INTO transaction_table (type, amount, content, date) " +
+                        "VALUES ('EXPENSE', 1000, '예시1 (지출)', $now)"
+            )
+
+            db.execSQL(
+                "INSERT INTO transaction_table (type, amount, content, date) " +
+                        "VALUES ('INCOME', 1000, '예시2 (수입)', $now)"
+            )
         }
     }
 }
